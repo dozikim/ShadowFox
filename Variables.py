b401a86537a3bbe5493e40eeb738cfe646496fba
Dozikim <bs524942@gmail.com>
2025-02-28 10:25:58 +0530

    Create Variables.py

---
+++ Variables.py
@@ -0,0 +1,15 @@
+# 1.1 Create a variable named pi and store the value 22/7
+pi = 22 / 7
+print("Value of pi:", pi)
+print("Data type of pi:", type(pi))
+
+# 1.2 Attempt to create a variable called 'for' (which is a reserved keyword)
+# Uncommenting the line below will raise a SyntaxError
+# for = 4  # 'for' is a reserved keyword in Python
+
+# 1.3 Calculate Simple Interest
+P = 1000  # Principal amount
+R = 5     # Rate of interest
+T = 3     # Time in years
+SI = (P * R * T) / 100
+print("Simple Interest:", SI)
